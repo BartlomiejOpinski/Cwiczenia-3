c1e4d5446f31de0d922d335d63146598b23fdd24
s18684 <s18684@pjwstk.edu.pl>
2024-04-05 14:45:45 +0200

    pierwszy commit

---
+++ LegacyAppTests/UserServiceTests.cs
@@ -0,0 +1,18 @@
+﻿using LegacyApp;
+using Xunit;
+
+namespace LegacyAppTests;
+
+public class UserServiceTests
+{
+    [Fact]
+    public void AddUser_Should_Return_False_When_Missing_FirstName()
+        {
+            var service = new UserService();
+
+            var result = service.AddUser(null, null, "kowalski@wp.pl", new DateTime(1980, 8, 18), 1);
+
+            Assert.Equal(false,result);
+        }
+    
+}
